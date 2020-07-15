bb16da774ee3b8dc12d4fe1a6402ecea44fac3f4
arindamdawn <arindamdawn3@gmail.com>
2020-07-15 14:13:58 +0530

    added post route

--- server.py
+++ server.py
@@ -1,12 +1,20 @@
 # import Flask class from flask  package
 from flask import Flask, render_template
 # create an instance of Flask class by providing the application module as parameter
-app = Flask(__name__) 
+app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
